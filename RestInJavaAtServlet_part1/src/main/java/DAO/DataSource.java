3d0d116b29adda3752f8049b40846a076d4dab8c
kosavpa <kosavpa@yandex.ru>
2022-12-27 12:37:53 +0400

    initialization pool connection

--- RestInJavaAtServlet_part1/src/main/java/DAO/DataSource.java
+++ RestInJavaAtServlet_part1/src/main/java/DAO/DataSource.java
@@ -1,40 +1,35 @@
 package DAO;
 
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
 import java.sql.*;
+
 
 public class DataSource {
     private static final String URL = "jdbc:mysql://localhost:3306/person";
     private static final String login = "root";
     private static final String password = "root";
-    private static java.sql.Connection connection;
+    private static BasicDataSource dataSource;
 
     static {
+        dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl(URL);
+        dataSource.setUsername(login);
+        dataSource.setPassword(password);
+
+        dataSource.setMinIdle(2);
+        dataSource.setMaxIdle(8);
+        dataSource.setMaxTotal(14);
+    }
+
+    public static Connection getConnection(){
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
-            connection = DriverManager.getConnection(URL, login, password);
-        } catch (InstantiationException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
+            return dataSource.getConnection();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
-    }
-
-    private DataSource() {
-    }
-
-    private static class InnerPersonDAOHolder{
-        public static final DataSource personDAO = new DataSource();
-    }
-
-    public static DataSource getInstance(){
-        return InnerPersonDAOHolder.personDAO;
-    }
-
-    public java.sql.Connection getConnection(){
-        return connection;
+        return null;
     }
 }
